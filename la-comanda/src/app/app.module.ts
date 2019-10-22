c1e0ba17f79ab05f0892214e5f52c5d60976c084
patoattie <patoattie@gmail.com>
2019-10-22 19:04:34 -0300

    arrancando el branch

--- la-comanda/src/app/app.module.ts
+++ la-comanda/src/app/app.module.ts
@@ -9,10 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from "../environments/environment";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
